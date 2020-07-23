bdb263ac386f0d4e0acdb870d3b1e2e01ce92cdb
Everton Sávio S. Lucas <everluca@outlook.com>
2020-07-23 13:52:18 -0300

    First Spark Java Project

---
+++ Spark_DataSets/JavaSpark_DataSets/01_Getting_Started/First_Project/src/main/java/Main.java
@@ -0,0 +1,34 @@
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.*;
+
+
+public class Main {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args)
+    {
+        System.setProperty("hadoop.home.dir", "c:/hadoop");
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]")
+                .config("spark.sql.warehouse.dir","file:///c:/tmp/")
+                .getOrCreate();
+
+        Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+
+        Dataset<Row> modernArtResults = dataset.filter(col("subject").equalTo("Modern Art")
+                .and(col("year").geq(2007)));
+
+        modernArtResults.show();
+
+        spark.close();
+
+    }
+
+}
