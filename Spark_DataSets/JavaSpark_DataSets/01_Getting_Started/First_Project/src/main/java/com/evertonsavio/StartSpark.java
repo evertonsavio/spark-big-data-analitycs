746e70ca27f947637bec07de91f7e83fb5fe1e33
Everton Sávio S. Lucas <everluca@outlook.com>
2020-07-23 15:39:02 -0300

    Spark Java Filters

--- Spark_DataSets/JavaSpark_DataSets/01_Getting_Started/First_Project/src/main/java/com/evertonsavio/StartSpark.java
+++ Spark_DataSets/JavaSpark_DataSets/01_Getting_Started/First_Project/src/main/java/com/evertonsavio/StartSpark.java
@@ -3,10 +3,12 @@ package com.evertonsavio;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
 import static org.apache.spark.sql.functions.*;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 
 public class StartSpark {
 
@@ -14,18 +16,43 @@ public class StartSpark {
     public static void main(String[] args) {
 
         System.setProperty("hadoop.home.dir", "c:/hadoop");
-        Logger.getLogger("org.apache").setLevel(Level.WARNING);
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkSession spark = SparkSession.builder().appName("FirstProject")
                 .master("local[*]")
                 .config("spark.sql.warehouse.dir","file:///c:/tmp/") // FOR WINDOWS USERS
                 .getOrCreate();
 
-        Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
-        dataset.show();
+        Dataset<Row> dataset = spark.read()
+                .option("header", true)
+                .csv("src/main/resources/exams/students.csv");
+
+        //dataset.show();
         //long numberOfRows = dataset.count();
         //System.out.println(numberOfRows);
-        //Dataset<Row> mathResult = dataset.filter(col("subject").equalTo("Math").and)
+        /////////////////////////////////////////////////////////////////////////
+        ///FILTER
+
+        Dataset<Row> mathResult = dataset.filter(col("subject")
+                .equalTo("Math")
+                .and(col("year").equalTo("2005")));
+
+        Dataset<Row> modernArtResults = dataset.filter("subject = 'Modern Art' AND year == 2007");
+
+        /////////////////////////////////////////////////////////////////////////
+        ///SHOW
+
+        modernArtResults.show();
+        mathResult.show();
+        /////////////////////////////////////////////////////////////////////////
+
+        Row firstRow = dataset.first();
+        //String subject = firstRow.get(2).toString();
+        String subject = firstRow.getAs("subject").toString();
+        System.out.println(subject);
+
+        int year = Integer.parseInt(firstRow.getAs("year").toString());
+        System.out.println("Year is :" + year);
 
         spark.close();
     }
