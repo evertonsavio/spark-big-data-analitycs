a646e44f60d4193063990be84eea3bb2cdea0bd5
Everton Sávio S. Lucas <everluca@outlook.com>
2020-07-27 20:08:20 -0300

    Pivot DataFrame Java

--- Spark_DataSets/JavaSpark_DataSets/00_Projects/src/main/java/com/evertonsavio/PivotTableSpark.java
+++ Spark_DataSets/JavaSpark_DataSets/00_Projects/src/main/java/com/evertonsavio/PivotTableSpark.java
@@ -6,10 +6,17 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
-// import org.apache.spark.sql.functions; // -> functions.date_format
-import static org.apache.spark.sql.functions.*; // -> date_format
 
-public class SQLToDataFrame {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.spark.sql.functions.*;
+
+// import org.apache.spark.sql.functions; // -> functions.date_format
+
+public class PivotTableSpark {
 
     public static void main(String[] args) {
 
@@ -23,15 +30,22 @@ public class SQLToDataFrame {
         Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/biglog.txt");
 
         //dataset = dataset.selectExpr("level", "date_format(datetime,'MMMM') as month");
-
+        
         dataset = dataset.select(col("level"),
                 date_format(col("datetime"), "MMMM").alias("month"),
                 date_format(col("datetime"), "M").alias("monthnum"));
 
-        dataset = dataset.groupBy(col("level"), col("month"))
-                .count()
-                .orderBy(first("monthnum").cast(DataTypes.IntegerType), first("level"));;
+        //List<Object> columns = new ArrayList<Object>();
+        //columns.add("March");
+        Object[] months = new Object[] {
+                "January", "AUHSNFJA", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+        };
+        List<Object> columns = Arrays.asList(months);
 
+        dataset = dataset.groupBy("level").pivot("month", columns)
+                .count()
+                .na()
+                .fill(0);
 
         dataset.show();
 
