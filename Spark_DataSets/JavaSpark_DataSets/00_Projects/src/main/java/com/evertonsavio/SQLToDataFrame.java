f01672a37145f9a7625a099dce9642fc0592da9f
Everton Sávio S. Lucas <everluca@outlook.com>
2020-07-27 19:42:07 -0300

    Ordering DataFrame Java

--- Spark_DataSets/JavaSpark_DataSets/00_Projects/src/main/java/com/evertonsavio/SQLToDataFrame.java
+++ Spark_DataSets/JavaSpark_DataSets/00_Projects/src/main/java/com/evertonsavio/SQLToDataFrame.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
 // import org.apache.spark.sql.functions; // -> functions.date_format
 import static org.apache.spark.sql.functions.*; // -> date_format
 
@@ -22,7 +23,16 @@ public class SQLToDataFrame {
         Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/biglog.txt");
 
         //dataset = dataset.selectExpr("level", "date_format(datetime,'MMMM') as month");
-        dataset = dataset.select(col("level"), date_format(col("datetime"), "MMMM").alias("month"));
+        
+        dataset = dataset.select(col("level"),
+                date_format(col("datetime"), "MMMM").alias("month"),
+                date_format(col("datetime"), "M").alias("monthnum"));
+
+        dataset = dataset.groupBy(col("level"), col("month"))
+                .count()
+                .orderBy(first("monthnum").cast(DataTypes.IntegerType), first("level"));;
+
+
         dataset.show();
 
         spark.close();
