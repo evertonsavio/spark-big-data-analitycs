7d6cc8365e54929721650c02e72d82c35527cd47
Everton Sávio S. Lucas <everluca@outlook.com>
2020-07-27 14:32:43 -0300

    InMemory Dataset plus Schema

---
+++ Spark_DataSets/JavaSpark_DataSets/00_Projects/src/main/java/com/evertonsavio/InMemorySpark.java
@@ -0,0 +1,46 @@
+package com.evertonsavio;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemorySpark {
+
+    public static void main(String[] args) {
+
+        System.setProperty("hadoop.home.dir", "c:/hadoop");
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]")
+                .config("spark.sql.warehouse.dir","file:///c:/tmp/")
+                .getOrCreate();
+
+        List<Row> inMemory = new ArrayList<Row>();
+
+        inMemory.add(RowFactory.create("WARN", "16 December 2018"));
+
+        StructField[] fields = new StructField[] {
+                new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+        };
+
+        StructType schema = new StructType(fields);
+
+        Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
+
+        dataset.show();
+
+        spark.close();
+
+    }
+}
