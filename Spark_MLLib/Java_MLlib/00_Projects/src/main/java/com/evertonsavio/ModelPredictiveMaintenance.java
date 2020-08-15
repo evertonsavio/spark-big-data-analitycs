1f27844f066bb543f827f1663d4aad5304dbff73
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-15 09:45:14 -0300

    Logistic Regression Spark Java

--- Spark_MLLib/Java_MLlib/00_Projects/src/main/java/com/evertonsavio/ModelPredictiveMaintenance.java
+++ Spark_MLLib/Java_MLlib/00_Projects/src/main/java/com/evertonsavio/ModelPredictiveMaintenance.java
@@ -2,9 +2,12 @@ package com.evertonsavio;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.*;
 
-public class PredictiveMaintenance {
+public class ModelPredictiveMaintenance {
 
     public static void main(String[] args) {
 
@@ -23,8 +26,14 @@ public class PredictiveMaintenance {
                 .option("url", "jdbc:sqlserver://localhost:1433;databaseName=OPCServiceBus")
                 //.option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
                 .option("dbtable", "dbo.df_2638_vib_imp")
+                .option("inferSchema", true)
                 .option("user", "sa")
                 .option("password", "sa").load();
+
+        dataset.createOrReplaceTempView("DataSet");
+
+        dataset = spark.sql("SELECT * from DataSet WHERE Label_VIb != 0 AND Label_VIb != 2");
+
 
         dataset.show();
     }
